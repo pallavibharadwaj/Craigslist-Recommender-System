9c90af9582bc01f404fa3af0b603297d743cdd4f
Harikrishna Karthikeyan <harikrishna_karthikeyan@sfu.ca>
2019-11-23 18:58:42 -0800

    Removing redundant entries

--- app/charts.py
+++ app/charts.py
@@ -44,27 +44,6 @@ class ChartData:
         resp = data
         return resp
 
-
-class ChartData:
-    def data1(self):
-        resp = [
-            ['ca-5682', 0],
-            ['ca-bc', 1],
-            ['ca-nu', 2],
-            ['ca-nt', 3],
-            ['ca-ab', 4],
-            ['ca-nl', 5],
-            ['ca-sk', 6],
-            ['ca-mb', 7],
-            ['ca-qc', 8],
-            ['ca-on', 9],
-            ['ca-nb', 10],
-            ['ca-ns', 11],
-            ['ca-pe', 12],
-            ['ca-yt', 13]
-        ]
-        return resp
-
     def data2(self):
         resp = [
             ['ca-5682', 0],
